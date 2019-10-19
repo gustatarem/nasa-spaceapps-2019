f321ab59cf6d050c93da9506a3fdd283519faf6e
Luiz Matias <luiz.matias1999@gmail.com>
2019-10-19 19:58:38 -0300

    Implemented API

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,8 +4,32 @@ import Manager from './app/models/Manager';
 import Cause from './app/models/Cause';
 import Demand from './app/models/Demand';
 
+import ManagerController from './app/controllers/ManagerController';
+import CauseController from './app/controllers/CauseController';
+import DemandController from './app/controllers/DemandController';
+
 const routes = new Router();
 
+
+//Managers
+//Get Manager
+routes.get('/managers/:id', ManagerController.index);
+
+//Causes
+//Get Causes
+routes.get('/causes', CauseController.show);
+//Get Specific Cause
+routes.get('/causes/:id', CauseController.index);
+
+//Demands
+//Get Demands based by causeId
+routes.get('/causes/:causeId/demands', DemandController.show);
+//Get Specific Demand
+routes.get('/demands/:id', DemandController.index);
+//Update Status From Specific Demand
+routes.put('/demands/:id', DemandController.updateStatus);
+
+//"Seeder"
 routes.get('/', async (req, res) => {
 
 	const user = await User.create({
